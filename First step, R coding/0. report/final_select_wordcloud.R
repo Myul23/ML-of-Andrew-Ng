ac140ed9a53a28d4f8e5f275618b1e374a2cbbbb
Myul23 <Myul23@users.noreply.github.com>
2020-11-02 00:27:17 +0900

    Update at before days

--- First step, R coding/0. report/final_select_wordcloud.R
+++ First step, R coding/0. report/final_select_wordcloud.R
@@ -1,5 +1,4 @@
-library(RCurl)
-# library(KoNLP)
+library(rvest)
 library(RmecabKo)
 library(RColorBrewer)
 library(wordcloud2)
@@ -7,31 +6,44 @@ library(wordcloud2)
 
 # base url setting
 urlPart1 = "https://search.naver.com/search.naver?&where=news"
-query = "&query=%EC%BD%94%EB%A1%9C%EB%82%98"
+query = "&query=코로나"
 urlPart2 = "&sm=tab_pge"
 sortNum = "&sort=0"
 newsType = "&photo=3"
 urlPart3 = "&field=0&reporter_article=&pd=3"
-date1 = "&ds=2020.03.01&de=2020.03.31"
+date1 = "&ds=2020.02.01&de=2020.02.29"
+# &ds=2020.03.01&de=2020.03.15
+# &ds=2020.03.16&de=2020.03.31
+# &ds=2020.04.01&de=2020.04.15
+# &ds=2020.04.16&de=2020.04.30
+# &ds=2020.05.01&de=2020.05.15
+# &ds=2020.05.16&de=2020.05.31
 urlPart4 = "&docid=&nso=so:r"
-date2 = ",p:from20200301to20200331"
+date2 = ",p:from20200201to20200229"
+# ,p:from20200301to20200315
+# ,p:from20200316to20200331
+# ,p:from20200401to20200415
+# ,p:from20200416to20200430
+# ,p:from20200501to20200515
+# ,p:from20200516to20200531
 urlPart5 = ",a:all&mynews=0&start="
 start = 0
 
 
 # 고통의 시작
-tit = c();  desc = c(); skip = c()
-# 3월: 27,580, 4월: 24,779, 5월: 21,343
-for (i in 0:275) {
+tit = c(); desc = c(); skip = c()
+# 3월: 27,580, 4월: 24,779, 5월: 21,343이었지만, 모두 4000개만
+for (i in 0:40) {
   start = i*100 + 1
   url = paste(urlPart1, query, urlPart2, sortNum, newsType, urlPart3, date1, urlPart4, date2, urlPart5, start, sep="")
   urlMoum = read_html(url)
   
-  # li#sp_nws1, (dt, a href)
-  middle_point = html_nodes(urlMoum, "div#container") %>% html_nodes("ul.type01") %>%
-                 html_nodes("dt") %>% html_nodes("a") %>% html_attr("href")
+  # /html/body/div[3]/div[2]/div/div[1]/section[1]/div/div[3]/ul/li[1]/div[1]/div/div[1]/div/a[2]
+  middle_point = html_nodes(urlMoum, "div#container") %>% html_nodes("div#main_pack") %>%
+                 html_nodes("ul.list_news") %>% html_nodes("div.news_area") %>% html_nodes("a.info") %>%
+                 html_attr("href")
   
-  for (j in 1:10) {
+  for (j in 1:length(middle_point)) {
     tryCatch({ element_base = read_html(middle_point[j]) },
         error = function(e) { skip = c(skip, middle_point[j]) })
     tryCatch({
@@ -44,33 +56,40 @@ for (i in 0:275) {
   print(start)
 }; rm(i); rm(j)
 
-# 본격적으로 워드 클라우드 그리기
+desc = c(tit, desc)
 print(length(desc))
-# nouns_base = extractNoun(desc)
-nouns_base = nouns(iconv(desc, "utf-8"))
 
-# extractNoun와 nouns의 인자 제한 범위를 훌쩍 넘겨 버려서 그걸 고쳐보고자 한 것들.
-# for (i in 1:6) { desc = c(desc, "") }
-# num = length(desc)/46
-# nouns_base = c()
-# for (i in 0:45) {
-#   result = nouns(iconv(desc[(i*num + 1):(i + 1)*num], "utf-8"))
-#   nouns_base = c(nouns_base, result)
-# }
+
+# 본격적으로 워드 클라우드 그리기
+for (i in 1:6) { desc = c(desc, "") }
+num = length(desc)/10
+
+nouns_base = c()
+for (i in 0:10) {
+  result = nouns(iconv(desc[(i*num + 1):(i + 1)*num], "utf-8"))
+  nouns_base = c(nouns_base, result)
+}
+# nouns_base = nouns(iconv(desc, "utf-8"))
 
 nouns_base = unique(nouns_base)
 nouns_base = unlist(nouns_base)
 nouns_base = nouns_base[nchar(nouns_base) >= 2]
 
 
-ele_nouns = gsub("\n", "", nouns_base) %>% gsub("\r", "") %>% gsub("http*", "") %>%
-            gsub("[[:punct:]]", "") %>% gsub("[A-Za-z]", "") %>% gsub("\\d+", "") %>%
-            gsub(" ", "", fixed = TRUE)
+ele_nouns = gsub("[[:cntrl:]]", "", nouns_base)
+ele_nouns = gsub("http*", "", ele_nouns)
+ele_nouns = gsub("[[:punct:]]", "", ele_nouns)
+ele_nouns = gsub("[A-Za-z]", "", ele_nouns)
+ele_nouns = gsub("\\d+", "", ele_nouns)
+ele_nouns = gsub(" ", "", fixed = TRUE, ele_nouns)
 
 # about COVID-19
-nouns = gsub("^신종.*", "", ele_nouns) %>% gsub("^코로나.*", "") %>%
-        gsub("^바이러스.*", "") %>% gsub("^감염증.*", "") %>%
-        gsub("확진자.*", "확진자") %>% gsub("감염자", "확진자")
+nouns = gsub("신종", "", ele_nouns)
+nouns = gsub("코로나", "", nouns)
+nouns = gsub("바이러스", "", nouns)
+nouns = gsub("감염증", "", nouns)
+nouns = gsub("확진자.*", "확진자", nouns)
+nouns = gsub("감염자", "확진자", nouns)
 
 
 final = nouns[nchar(nouns) >= 2]
